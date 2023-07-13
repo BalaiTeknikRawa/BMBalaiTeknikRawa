93ef441bb7709ef589569540169f96e502e5fde5
BalaiTeknikRawa <139412532+BalaiTeknikRawa@users.noreply.github.com>
2023-07-13 11:15:06 +0800

    Add files via upload

---
+++ data/POINTBMBTR49S_3.js
@@ -0,0 +1,8 @@
+var json_POINTBMBTR49S_3 = {
+"type": "FeatureCollection",
+"name": "POINTBMBTR49S_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Titik": "BM Eks PLG", "Lokasi": "Papuyu, Kahayan Pisau, Pulang Pisau, Kalimantan Tengah", "Tahun ": "2022", "X": 832136.189, "Y": 9629292.191, "Zona": "49S", "Orthometri": null, "Tgl Monev": "31 Mei 2022", "Deskripsi": "https:\/\/drive.google.com\/file\/d\/1BwGOeGs1phEU0yi5TdigOVDOEgIdqeek\/view?usp=drive_link", "Keterangan": null, "Inacors": null, "Auspost": "2,651" }, "geometry": { "type": "Point", "coordinates": [ 113.988541069547495, -3.349289875134742 ] } }
+]
+}
---
+++ data/POINTBMBTR50N_1.js
@@ -0,0 +1,8 @@
+var json_POINTBMBTR50N_1 = {
+"type": "FeatureCollection",
+"name": "POINTBMBTR50N_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Titik": "BM SP 06 B BTR", "Lokasi": "Tanjung Buka, Bulungan, Kalimantan Utara", "Tahun": "2022", "X": 548378.0, "Y": 321930.0, "Zona": "50N", "Orthometri": "1,710", "Tgl Monev": null, "Deskripsi": "https:\/\/drive.google.com\/file\/d\/1sWMmVj7GZvxIYi5IN0BH_0z7xS6Qoaca\/view?usp=drive_link", "Keterangan": null, "Inacors": null, "Auspost": null }, "geometry": { "type": "Point", "coordinates": [ 117.435315221755701, 2.912492035651939 ] } }
+]
+}
